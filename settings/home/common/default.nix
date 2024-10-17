7ad6c6259fe0d76fe794e344c12fada8d415da5c
gamoutatsumi <tatsumi@gamou.dev>
2024-10-17 14:15:49 +0900

    update

--- settings/home/common/default.nix
+++ settings/home/common/default.nix
@@ -39,7 +39,7 @@ in
         url = "https://raw.githubusercontent.com/bluz71/fly16-bat/master/fly16.tmTheme";
         sha256 = "0xp10xdcsnfpwzhwpjj4jsgwjkzpaln2d2gc9iznfzkq95a9njs8";
       };
-      "git/tempalte/hooks/pre-push".source = builtins.fetchurl {
+      "git/template/hooks/pre-push".source = builtins.fetchurl {
         url = "https://gist.githubusercontent.com/quintok/815396509ff79d886656b2855e1a8a46/raw/e6770add98e7db57177c16d33be31bfdf2c23042/pre-push";
 
         sha256 = "0lynwihky1b6r8ssjzzw99p38vya39x204gknpi2fdg720jj87yj";
