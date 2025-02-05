7022f0953b5739ca98695726ffbaa946688c0fee
gamoutatsumi <tatsumi@gamou.dev>
2025-02-05 11:57:35 +0900

    update

--- settings/home/common/config/nvim/dpp/nvim_lsp.lua
+++ settings/home/common/config/nvim/dpp/nvim_lsp.lua
@@ -128,7 +128,7 @@ lspconfig.ts_ls.setup({
     autostart = true,
 })
 
-if vim.fn.executable("typescript-language-server") == 0 then
+if vim.fn.executable("typescript-language-server") == 0 or vim.fn.executable("vtsls") == 0 then
     lspconfig.denols.setup({
         on_attach = on_attach,
         autostart = true,
