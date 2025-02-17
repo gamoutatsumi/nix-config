4f9a91fa50b5ac59239d5d5108c34d74f1aac28e
gamoutatsumi <tatsumi@gamou.dev>
2025-02-17 20:46:22 +0900

    update

--- flake.nix
+++ flake.nix
@@ -522,7 +522,7 @@
             # keep-sorted end
           ] ++ lib.optionals stdenv.isLinux [ deno-overlay.overlays.deno-overlay ];
         });
-      denoVersion = "2.1.9";
+      denoVersion = "2.1.10";
       homeManagerConf =
         {
           imports,
