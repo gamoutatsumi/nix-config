c65f7300575b0d79b8ebaddf868486e3723e9d8c
gamoutatsumi <tatsumi@gamou.dev>
2025-02-26 14:17:14 +0900

    update

--- settings/home/common/config/nvim/dpp/avante.lua
+++ settings/home/common/config/nvim/dpp/avante.lua
@@ -3,7 +3,7 @@ require("avante").setup({
     provider = "copilot",
     auto_suggestions_provider = "copilot",
     copilot = {
-        model = "claude-3-7-sonnet-20250219",
+        model = "claude-3.7-sonnet",
     },
     behaviour = {
         auto_suggestions = false,
