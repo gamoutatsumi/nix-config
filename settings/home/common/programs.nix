7cc890a2d7d2d0bcee7d9dec6a64794fb9a24cf3
gamoutatsumi <tatsumi@gamou.dev>
2025-03-12 11:07:26 +0900

    update

--- settings/home/common/programs.nix
+++ settings/home/common/programs.nix
@@ -441,7 +441,6 @@ in
         MYCLI_HISTFILE = "${config.xdg.dataHome}/mycli/history";
         NIX_CONFIG = "access-tokens = github.com=$(gh auth token)";
         PAGER = "${pkgs.ov}/bin/ov";
-        POWERLEVEL9K_TERM_SHELL_INTEGRATION = "true";
         PURE_GIT_PULL = 0;
         TERM = "alacritty";
         WORDCHARS = "*?_.[]~-=&;!#$%^(){}<>";
