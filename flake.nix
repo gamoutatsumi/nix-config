d9815d1a5f7555f99d66961e1b18d78d570078bd
gamoutatsumi <tatsumi@gamou.dev>
2025-04-10 21:45:51 +0900

    update

--- flake.nix
+++ flake.nix
@@ -255,7 +255,7 @@
       };
     };
     mcp-hub = {
-      url = "github:ravitemer/mcp-hub/v2.1.1";
+      url = "github:ravitemer/mcp-hub/3381c41e74f00e6479005b838b44b98484be2644";
       inputs = {
         nixpkgs = {
           follows = "nixpkgs-unstable";
