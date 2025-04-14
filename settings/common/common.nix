33db9167c8e3d9fbabe88e5639918f5f3f735e54
gamoutatsumi <tatsumi@gamou.dev>
2025-04-14 13:11:15 +0900

    update

--- settings/common/common.nix
+++ settings/common/common.nix
@@ -35,6 +35,8 @@
       };
     };
     settings = {
+      substituters = [ "https://nix-community.cachix.org" ];
+      trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
       accept-flake-config = true;
       experimental-features = [
         "nix-command"
