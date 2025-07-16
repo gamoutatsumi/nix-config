7a9862be6bc35193e47390478624ae432751bd1c
gamoutatsumi <tatsumi@gamou.dev>
2025-07-16 12:41:37 +0900

    update

--- flake.nix
+++ flake.nix
@@ -5,11 +5,13 @@
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
       "https://cache.garnix.io"
+      "https://gamoutatsumi-nix-config.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "gamoutatsumi-nix-config.cachix.org-1:5quoMby5QSByFx7JxJy75/JOkMmvbD314bwph5BToSw="
     ];
   };
   inputs = {
