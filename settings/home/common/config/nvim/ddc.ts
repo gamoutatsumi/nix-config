28b8f9629485727e4657305e2cce46daa8671c28
gamoutatsumi <tatsumi@gamou.dev>
2025-08-25 14:07:54 +0900

    update

--- settings/home/common/config/nvim/ddc.ts
+++ settings/home/common/config/nvim/ddc.ts
@@ -1,7 +1,7 @@
 import {
   BaseConfig,
   type ConfigArguments,
-} from "jsr:@shougo/ddc-vim@9.5.0/config";
+} from "jsr:@shougo/ddc-vim@10.0.0/config";
 
 const CONVERTERS = [
   "converter_fuzzy",
