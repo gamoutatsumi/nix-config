7af97a78bdd4d70057c8392386d6b4284620b704
gamoutatsumi <tatsumi@gamou.dev>
2025-09-03 15:50:06 +0900

    update

--- settings/home/common/config/nvim/after/lsp/yamlls.lua
+++ settings/home/common/config/nvim/after/lsp/yamlls.lua
@@ -8,6 +8,7 @@ return {
                 ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
                 ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.24.3-standalone-strict/all.json"] = "/*.k8s.yaml",
                 ["https://www.schemastore.org/github-action.json"] = "action.{yml,yaml}",
+                ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/tasks"] = "/roles/*/tasks/*.{yml,yaml}",
             },
         },
     },
