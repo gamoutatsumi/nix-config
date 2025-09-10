ee3e2e07d9d52e251bd551c1386525da998491b2
gamoutatsumi <tatsumi@gamou.dev>
2025-09-11 08:49:00 +0900

    update

--- settings/home/common/config/nvim/after/lsp/texlab.lua
+++ settings/home/common/config/nvim/after/lsp/texlab.lua
@@ -5,12 +5,12 @@ return {
             build = {
                 executable = "lualatex",
                 args = { "-synctex=1", "%f" },
-                onSave = false,
-                forwardSearchAfter = false,
+                onSave = true,
+                forwardSearchAfter = true,
             },
             forwardSearch = {
                 executable = "zathura",
-                args = { "--synctex-forward", "%l%1:%f", "%p" },
+                args = { "--synctex-forward", [[%l:1:%f]], "%p" },
             },
         },
     },
