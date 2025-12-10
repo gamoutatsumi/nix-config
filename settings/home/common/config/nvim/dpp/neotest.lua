9d3e6ad768b49d4d7e97f89ef39035782e7ea6fd
gamoutatsumi <tatsumi@gamou.dev>
2025-12-10 17:37:02 +0900

    update

--- settings/home/common/config/nvim/dpp/neotest.lua
+++ settings/home/common/config/nvim/dpp/neotest.lua
@@ -1,5 +1,6 @@
 -- lua_add {{{
-require("neotest").setup({
+local neotest = require("neotest")
+neotest.setup({
     adapters = {
         require("neotest-golang")({
             testify_enabled = true,
@@ -14,4 +15,16 @@ require("neotest").setup({
         require("neotest-plenary"),
     },
 })
+
+vim.keymap.set("n", "<leader>tt", neotest.run.run, { desc = "Run nearest teset" })
+vim.keymap.set("n", "<leasder>tf", function()
+    neotest.run.run(vim.fn.expand("%"))
+end, { desc = "Run file tests" })
+vim.keymap.set("n", "<leader>ts", neotest.summary.toggle, { desc = "Toggle test summary" })
+vim.keymap.set("n", "<leader>to", function()
+    neotest.output.open({ enter = true })
+end, { desc = "Open test output" })
+vim.keymap.set("n", "<leader>td", function()
+    neotest.run.run({ suite = false, strategy = "dap" })
+end, { desc = "Debug nearest test" })
 -- }}}
