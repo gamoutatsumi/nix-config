b6d8d7d060196a7e78f67e4a3e2eff444fdbd10d
gamoutatsumi <tatsumi@gamou.dev>
2026-01-07 11:01:25 +0900

    update

--- flake/parts/home/linux/wayland.nix
+++ flake/parts/home/linux/wayland.nix
@@ -39,12 +39,22 @@ in
             "mpris"
           ];
           modules-right = [
+            "battery"
             "pulseaudio"
             "cpu"
             "memory"
             "tray"
             "clock"
           ];
+          battery = {
+            states = {
+              warning = 30;
+              critical = 15;
+            };
+            format = "{icon} {capacity}%";
+            format-charging = "{capacity}%";
+            format-plugged = "{capacity}%";
+          };
           mpris = {
             format = " {player}: {player_icon} {dynamic}";
             format-paused = " {player}: {status_icon} <i>{dynamic}</i>";
