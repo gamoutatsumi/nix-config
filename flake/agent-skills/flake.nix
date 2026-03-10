0a19418301a7fa64b899f39684e8f0045f2bfa74
gamoutatsumi <tatsumi@gamou.dev>
2026-03-10 12:12:50 +0900

    update

--- flake/agent-skills/flake.nix
+++ flake/agent-skills/flake.nix
@@ -3,6 +3,10 @@
     nixpkgs = {
       url = "github:NixOS/nixpkgs?ref=nixos-25.11&shallow=1";
     };
+    ast-grep-source = {
+      url = "github:ast-grep/agent-skill";
+      flake = false;
+    };
     home-manager = {
       url = "github:nix-community/home-manager?ref=release-25.11";
       inputs = {
@@ -36,53 +40,78 @@
     {
       agent-skills,
       anthropic-skills,
+      ast-grep-source,
       hashicorp-skills,
       ...
     }:
     {
       homeManagerModules = {
-        default = {
-          imports = [
-            agent-skills.homeManagerModules.default
-          ];
+        default =
+          { pkgs, ... }:
+          {
+            imports = [
+              agent-skills.homeManagerModules.default
+            ];
 
-          programs.agent-skills = {
-            enable = true;
-            sources = {
-              anthropic = {
-                path = anthropic-skills;
-                subdir = "skills";
-              };
-              terraform-provider = {
-                path = hashicorp-skills;
-                subdir = "terraform/provider-development";
-              };
-              personal = {
-                path = ./skills;
-              };
-            };
-            skills = {
-              enable = [
-                "frontend-design"
-                "skill-creator"
-              ];
-              enableAll = [
-                "personal"
-                "terraform-provider"
-              ];
-            };
-            targets = {
-              codex = {
-                dest = ".codex/skills";
-                structure = "link";
-              };
-              claude = {
-                dest = ".claude/skills";
-                structure = "link";
+            programs = {
+              agent-skills = {
+                enable = true;
+                sources = {
+                  anthropic = {
+                    path = anthropic-skills;
+                    subdir = "skills";
+                  };
+                  terraform-provider = {
+                    path = hashicorp-skills;
+                    subdir = "terraform/provider-development/skills";
+                  };
+                  ast-grep-source = {
+                    path = ast-grep-source;
+                    subdir = "ast-grep/skills";
+                  };
+                  personal = {
+                    path = ./skills;
+                  };
+                };
+                skills = {
+                  explicit = {
+                    ast-grep = {
+                      from = "ast-grep-source";
+                      path = "ast-grep";
+                      packages = with pkgs; [ ast-grep ];
+                      transform =
+                        { original, dependencies }:
+                        ''
+                          ${original}
+
+                          ${dependencies}
+
+                          - You should use ./ast-grep for execution.
+                        '';
+                    };
+                  };
+                  enable = [
+                    "frontend-design"
+                    "skill-creator"
+                  ];
+                  enableAll = [
+                    "personal"
+                    "terraform-provider"
+                  ];
+                };
+                targets = {
+                  codex = {
+                    dest = ".codex/skills";
+                    structure = "link";
+                  };
+                  claude = {
+                    dest = ".claude/skills";
+                    structure = "link";
+                  };
+                };
               };
             };
           };
-        };
       };
     };
 }
