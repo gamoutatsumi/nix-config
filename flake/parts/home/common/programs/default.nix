5a6ea9bd383c82185c3fdae275a0a15371b5a82b
gamoutatsumi <tatsumi@gamou.dev>
2026-04-08 13:19:30 +0900

    update

--- flake/parts/home/common/programs/default.nix
+++ flake/parts/home/common/programs/default.nix
@@ -149,6 +149,9 @@ in
     };
     direnv = {
       enable = true;
+      package = pkgs.direnv.overrideAttrs (_: {
+        doCheck = false;
+      });
       enableZshIntegration = true;
       nix-direnv = {
         enable = true;
