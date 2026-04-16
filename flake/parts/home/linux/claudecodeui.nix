598525232086c85ded0e14d466e70d21b5e4e06c
gamoutatsumi <tatsumi@gamou.dev>
2026-04-16 10:04:42 +0900

    update

--- flake/parts/home/linux/claudecodeui.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, buildNpmPackage, ... }:
-let
-  inherit ((pkgs.callPackage ../../../../_sources/generated.nix { })) claudecodeui;
-in
-buildNpmPackage {
-  inherit (claudecodeui) src version pname;
-  nativeBuildInputs = with pkgs; [ makeWrapper ];
-  npmDepsHash = "sha256-YiwuHSW3oy4HsDaTkzJMijaBjCIKtTBj3PvwDR8sFGQ=";
-}
